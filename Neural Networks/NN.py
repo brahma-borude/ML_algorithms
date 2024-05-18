193f46b4c1b4177370dc6d8394b7c4ede7aa2fc3
Brahma <146714243+brahma-borude@users.noreply.github.com>
2024-05-18 22:22:03 +0530

    Update NN.py

--- Neural Networks/NN.py
+++ Neural Networks/NN.py
@@ -33,11 +33,13 @@ class NeuralNet(nn.Module):
     def __init__(self, input_size, num_classes):
         super(NeuralNet, self).__init__()
         self.fc1 = nn.Linear(in_features=input_size, out_features=16)
-        self.fc2 = nn.Linear(in_features=16, out_features=num_classes)
+        self.fc2 = nn.Linear(in_features=16, out_features=16)
+        self.fc3 = nn.Linear(in_features=16, out_features=num_classes)
         
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         
         return x
 
